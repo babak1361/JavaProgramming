ae33ae1b60005357151cd6ea6cc435532e5567cd
babak <babak.nikahval.bn@gmail.com>
2022-01-13 11:48:05 -0500

    Parking Lot

---
+++ src/day38_Inheritance/cartask/ParkingLot.java
@@ -0,0 +1,12 @@
+package day38_Inheritance.cartask;
+
+public class ParkingLot {// has relation
+    public static void main(String[] args) {
+
+        Toyota toyota =new Toyota("Camry",2020,20000,"Gray",45000);
+
+        Tesla tesla =new Tesla("S Moddel",2022,45600,"Red",12000);
+
+        BMW bmw =new BMW("X5",2020,48000,"Black",14000);
+    }
+}
