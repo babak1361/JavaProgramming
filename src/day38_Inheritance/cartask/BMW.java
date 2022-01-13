240aacb1e3b63f099954306c8e5992394f5503b8
babak <babak.nikahval.bn@gmail.com>
2022-01-13 11:00:08 -0500

    BMW

--- src/day38_Inheritance/cartask/BMW.java
+++ src/day38_Inheritance/cartask/BMW.java
@@ -1,4 +1,24 @@
 package day38_Inheritance.cartask;
 
-public class BMW {
+public class BMW extends Car {
+
+    public BMW( String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+    public void breakDown(){
+        System.out.println(brand+" "+model+" is breakDown");
+    }
+
+    public void racing(){
+        System.out.println(brand+" "+model+" is racing");
+    }
+
+
 }
+/*
+ 2. BMW:
+                extra methods:
+                    breaksDown()
+                    racing()
+ */
