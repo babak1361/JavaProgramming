2b335ef58c0b17ad88c8b3607fa4099ab4dc5482
babak <babak.nikahval.bn@gmail.com>
2022-01-13 11:03:14 -0500

    Tesla

--- src/day38_Inheritance/cartask/Tesla.java
+++ src/day38_Inheritance/cartask/Tesla.java
@@ -1,4 +1,17 @@
 package day38_Inheritance.cartask;
 
-public class Tesla {
+public class Tesla extends Car {
+    public Tesla( String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+
+    public void autopilot(){
+        System.out.println(brand+" "+model+" is autopilot mode");
+    }
+
 }
+/*
+ 3. Tesla:
+                extra methods:
+                    autoPilot();
+ */
